8c2bee006ed28e520d8ae0bddec34132a79fc53e
diego florez <dflorez@SURCO.com>
2020-03-06 15:36:15 -0500

    commt

--- src/reportenuevo/ReporteNuevo.java
+++ src/reportenuevo/ReporteNuevo.java
@@ -41,51 +41,48 @@ public class ReporteNuevo {
     
     public static void main(String[] args) {
         try {
-//            JasperReport jr = (JasperReport) 
-//            JRLoader.loadObject
-//            (ReporteNuevo.class.getResource("./reporte.jasper"));
-//            Map parametros = new HashMap<String, Object>();
-//            parametros.put("PRECIO", 1200);
-//            parametros.put("PRECIO2", 3300);
-//            JasperPrint jp = JasperFillManager.fillReport
-//               (jr, parametros, conectar());
-//            JasperViewer jv = new JasperViewer(jp);
-//            jv.show();
+            JasperReport jr = (JasperReport) JRLoader.loadObject(ReporteNuevo.class.getResource("./reporte.jasper"));
+            Map parametros = new HashMap<String, Object>();
+            parametros.put("PRECIO", 1200);
+            parametros.put("PRECIO2", 3300);
+            JasperPrint jp = JasperFillManager.fillReport(jr, parametros, conectar());
+            JasperViewer jv = new JasperViewer(jp);
+            jv.show();
 
-            JFrame f;
-            Statement statement = conectar().createStatement();
-            String query = "SELECT * FROM producto";
-            ResultSet resultSet = statement.executeQuery(query);
-            
-            DefaultTableModel model = new DefaultTableModel(new String[]{"Código", "Producto", "Precio"}, 0);
-            while(resultSet.next()) {
-                int idProd = resultSet.getInt("id_producto");
-                String descProd = resultSet.getString("desc_producto");
-                int precio = resultSet.getInt("precio");
-                model.addRow(new Object[]{idProd, descProd, precio});
-            }
-            JTable table = new JTable(model);
-            
-            f = new JFrame();
-            f.setTitle("JTable Example");
-            
-            JTextField txtNombre = new JTextField("");
-            JTextField txtPrecio = new JTextField("");
-            JButton button3 = new JButton("Registrar");
-            
-            table.setBounds(10, 40, 200, 300);
-            //JScrollPane sp = new JScrollPane(table);
-            //sp.add(button);
-            f.setLayout(new GridLayout(3,1));
-            Panel p1 = new Panel();
-            p1.setSize(new Dimension(700, 80));
-            p1.setLayout(new GridLayout(1,3));
-            p1.add(txtNombre);p1.add(txtPrecio);p1.add(button3);
-            
-            f.add(p1);
-            f.add(table);
-            f.setSize(700, 400);
-            f.setVisible(true);
+//            JFrame f;
+//            Statement statement = conectar().createStatement();
+//            String query = "SELECT * FROM producto";
+//            ResultSet resultSet = statement.executeQuery(query);
+//            
+//            DefaultTableModel model = new DefaultTableModel(new String[]{"Código", "Producto", "Precio"}, 0);
+//            while(resultSet.next()) {
+//                int idProd = resultSet.getInt("id_producto");
+//                String descProd = resultSet.getString("desc_producto");
+//                int precio = resultSet.getInt("precio");
+//                model.addRow(new Object[]{idProd, descProd, precio});
+//            }
+//            JTable table = new JTable(model);
+//            
+//            f = new JFrame();
+//            f.setTitle("JTable Example");
+//            
+//            JTextField txtNombre = new JTextField("");
+//            JTextField txtPrecio = new JTextField("");
+//            JButton button3 = new JButton("Registrar");
+//            
+//            table.setBounds(10, 40, 200, 300);
+//            //JScrollPane sp = new JScrollPane(table);
+//            //sp.add(button);
+//            f.setLayout(new GridLayout(3,1));
+//            Panel p1 = new Panel();
+//            p1.setSize(new Dimension(700, 80));
+//            p1.setLayout(new GridLayout(1,3));
+//            p1.add(txtNombre);p1.add(txtPrecio);p1.add(button3);
+//            
+//            f.add(p1);
+//            f.add(table);
+//            f.setSize(700, 400);
+//            f.setVisible(true);
             
         } catch (Exception e) {
             System.err.println(e);
